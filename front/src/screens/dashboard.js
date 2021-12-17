51014f6231c25c849dc638f3007ee5e88f0be4a0
Michael Dinglis <66019398+MichaelDinglis@users.noreply.github.com>
2021-12-17 17:27:41 +0200

    Add files via upload

---
+++ front/src/screens/dashboard.js
@@ -0,0 +1,164 @@
+import React,{useState} from 'react'
+import './dashboard.css'
+import {useSelector} from 'react-redux'
+import Axios from 'axios'
+import {useDispatch} from 'react-redux'
+import {removeUser} from '../redux/index'
+import { useHistory } from "react-router-dom";
+import moment from 'moment'
+import { withRouter } from "react-router-dom";
+import logo from '../logos/croppedTrans.png';
+require('dotenv').config()
+
+
+function Dashboard() {
+    const USER=useSelector(state => state.user)
+    const [user,setUser]=useState(USER[0])
+    const [comments,setComments]=useState("")
+    const [disableButtons,setDisablebuttons]=useState(false)
+
+    var lastcheck
+    if(USER[0].lastCheckIn!=null){
+        lastcheck=USER[0].lastCheckIn.replace('Z', '').replace('T', ' ');
+        }
+    const [lastCheckIn,setLastCheckIn]=useState(lastcheck)
+    const dispatch=useDispatch()
+    const history=useHistory()
+
+    const enableButtons=async()=>{
+        setDisablebuttons(false)
+    }
+
+    const checkIn=()=>{
+        setDisablebuttons(true)
+        if(lastCheckIn!= null){
+            alert("Please check out before checking in!")
+            enableButtons()
+        }
+        else{
+            const date=new Date().toISOString().slice(0, 10)
+            const time=new Date().toLocaleTimeString('el-GR',{hour12:false})
+            const datetime= `${date} ${time}`
+            Axios.post(process.env.REACT_APP_API+"checkinpost",{
+                username:USER[0].username,
+                datetime:datetime
+            }).then((res)=>{
+                alert(res.data.checkMessage)
+                setComments("")
+                setLastCheckIn(datetime)
+                enableButtons()
+            }).catch((err)=>{
+                alert(`Error! ${err}`)
+                enableButtons()
+            })
+        }
+    }
+
+    const done=()=>{
+        history.push("/")
+        dispatch(removeUser())
+    }
+
+    const checkOut=()=>{
+        setDisablebuttons(true)
+        const date=new Date().toISOString().slice(0, 10)
+        const time=new Date().toLocaleTimeString('el-GR',{hour12:false})
+        const timestampNow= `${date} ${time}`
+        Axios.get(`${process.env.REACT_APP_API}getCheckIn?username=${user.username}`).then((Resp)=>{
+            const data=Resp.data[0]
+            if(data.lastCheckIn===null){
+                alert("Please check in before checking out!")
+                enableButtons()
+            }
+            else{
+                const checkIn=data.lastCheckIn.replace('Z', '').replace('T', ' ');
+                setLastCheckIn(null)
+                const ms=moment(timestampNow,"YYYY/MM/DD HH:mm:ss").diff(moment(checkIn,"YYYY/MM/DD HH:mm:ss"))
+                const d = moment.duration(ms);
+                const totalHours=Math.floor(d.asHours()) + moment.utc(ms).format(".mm");                
+                Axios.post(process.env.REACT_APP_API+'checkOut',{
+                    username:user.username,
+                    checkIn:checkIn,
+                    checkOut:timestampNow,
+                    totalHours:totalHours,
+                    comments:comments
+    
+                    }).then((res)=>{
+                        if(res.data.checkMessage){
+                            alert(res.data.checkMessage)
+                        }
+                        else{
+                            alert(`Check Out Sucessfull! Worktime: ${totalHours}`)
+                            setComments("")
+                        }
+                        enableButtons()
+                    }).catch((e)=>{
+                        alert(JSON.stringify(e.message))
+                        enableButtons()
+                })
+            }
+        }).catch((e)=>{
+            alert(JSON.stringify(e.message))
+            enableButtons()
+        })
+    }
+
+    const handleComments=(e)=>{       
+            setComments(e.target.value)
+    }
+
+    const toManageUser=()=>{
+        history.push("/manageUsers")
+    }
+
+    const toReports=()=>{
+        history.push("/reports")
+    }
+
+    const toCreateUser=()=>{
+        history.push("createUser")
+    }
+
+    return (
+        <div className="mainDiv">
+            <div>
+                {user.role!=="employee" && (<div className="sidenav">
+                    <div className="menuName">{user.role.toUpperCase()} MENU</div>
+                    <a onClick={toCreateUser}>Create User</a>
+                    <a onClick={toManageUser}>Manage Users</a>
+                    <a onClick={toReports}>Reports</a>
+                </div>)}
+                <div className={user.role!=="employee" ? 'notCenterMain':'centerMain'}>
+                    <p className="welcomeTextDashboard">
+                    <img src={logo} className='topLogoDash'></img><br/>
+                        Welcome, {user.firstname}<br/><br/>
+                        Last CheckIn: {lastCheckIn ? lastCheckIn : "NULL"}
+                    </p>
+                    <hr/>
+                        <p className='currentStats'>
+                            Current date: {new Date().toISOString().slice(0, 10)}<br/>
+                            Current time: {new Date().toLocaleTimeString('el-GR',{hour12:false})}
+                        </p>
+                        <button disabled={disableButtons} onClick={()=>{checkIn()}}id="in" className="BUTTONS" >Check In</button>
+                        <button disabled={disableButtons} onClick={()=>{checkOut()}}id="out" className="BUTTONS">Check Out</button>
+                        <br/>
+                        <input id="commentBox" type="text" name="comments" value={comments} onChange={(e)=>{handleComments(e)}} placeholder="Checkout Comments"/>
+                        <br/>
+                        <button onClick={()=>{done()}} id="dashboardDoneButton">Done!</button><br/><br/>
+                    </div>
+            </div>
+
+
+            
+        </div>
+        
+    )
+}
+
+export default withRouter(Dashboard)
+
+
+{/* <div className='bottomStatDiv'>
+Current date: {new Date().toISOString().slice(0, 10)}<br/>
+Current time: {new Date().toLocaleTimeString('el-GR',{hour12:false})}
+</div> */}
---
+++ front/src/screens/err404.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {MdOutlineSentimentVeryDissatisfied} from 'react-icons/md'
+import { useHistory } from "react-router-dom";
+import logo from '../logos/croppedTrans.png';
+
+
+
+export default function Error404() {
+    const history=useHistory()
+
+    const backToHome=()=>{
+        history.push("/")
+    }
+    return (
+        <div>
+            <br/>
+            <img src={logo} className='topLogoError'></img><br/>
+            <MdOutlineSentimentVeryDissatisfied style={{width:'200px', height:'100px'}}></MdOutlineSentimentVeryDissatisfied><br></br>
+            <p style={{fontSize:'30px'}}>Error 404! Wrong Route!</p>
+            <button onClick={backToHome} className='backToHomeErrorPage'>Back to home</button>
+        </div>
+    )
+}
