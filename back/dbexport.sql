26b2079e92ee77e316f6148f6b5f9f510320468d
Michael Dinglis <66019398+MichaelDinglis@users.noreply.github.com>
2022-04-03 12:08:33 +0300

    Add files via upload

---
+++ back/dbexport.sql
@@ -0,0 +1,111 @@
+-- MySQL dump 10.13  Distrib 5.7.36, for Linux (x86_64)
+--
+-- Host: localhost    Database: hourcheck
+-- ------------------------------------------------------
+-- Server version	5.7.36-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Users` (
+  `firstname` varchar(30) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` int(10) NOT NULL,
+  `lastCheckIn` varchar(25) DEFAULT NULL,
+  `role` varchar(20) NOT NULL,
+  PRIMARY KEY (`username`),
+  KEY `role_constraint` (`role`),
+  CONSTRAINT `role_constraint` FOREIGN KEY (`role`) REFERENCES `roles` (`role`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES ('superUser','superUser',4,NULL,'supervisor');
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `hours`
+--
+
+DROP TABLE IF EXISTS `hours`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `checkIn` varchar(25) DEFAULT NULL,
+  `checkOut` varchar(25) DEFAULT NULL,
+  `totalHours` varchar(10) DEFAULT NULL,
+  `comments` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `username_index` (`username`),
+  CONSTRAINT `username_constraint` FOREIGN KEY (`username`) REFERENCES `Users` (`username`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hours`
+--
+
+LOCK TABLES `hours` WRITE;
+/*!40000 ALTER TABLE `hours` DISABLE KEYS */;
+/*!40000 ALTER TABLE `hours` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `roles` (
+  `role` varchar(20) NOT NULL,
+  `password` varchar(25) DEFAULT NULL,
+  PRIMARY KEY (`role`),
+  KEY `role_index` (`role`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES ('employee',NULL),('manager',NULL),('supervisor','lo');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- Dump completed on 2021-12-06 21:20:31
+-- INSERT INTO users (firstname,username,password,role) VALUES ('superUser','superUser',4,'supervisor');
+
